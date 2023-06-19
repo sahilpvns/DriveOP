34c1515e6c14871418d182e441c81b359a3f800f
Sahil <sahil.p@supplyop.com>
2023-06-19 12:21:52 +0530

    api url change

--- app/src/main/java/com/supplyop/driveop/login/api/method/UserApi.kt
+++ app/src/main/java/com/supplyop/driveop/login/api/method/UserApi.kt
@@ -6,7 +6,7 @@ import retrofit2.http.POST
 
 interface UserApi {
 
-    @POST("http://3.221.107.149:8080/supplyOp/login/genrate_token")
+    @POST("http://3.221.107.149:8080/supply_op/login/genrate_token")
     suspend fun loginUser(@Body loginRequest: LoginRequest): Response<LoginResponse>
 
     companion object {
