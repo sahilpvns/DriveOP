d3758ad05071f9ddf98c6e805a2ab420a8cca370
sahil <sahil.p@supplyop.com>
2023-07-13 17:25:22 +0530

    Move file.

--- app/src/main/java/com/supplyop/driveop/login/network/RetrofitInstance.kt
+++ app/src/main/java/com/supplyop/driveop/login/network/RetrofitInstance.kt
@@ -1,24 +1,38 @@
 package com.supplyop.driveop.login.network
 
 import com.google.gson.GsonBuilder
+import com.supplyop.driveop.login.utils.Constant
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
+
     var BASE_URL: String = "https://private-a27f13-sahilprajapati1.apiary-mock.com/"
-    val getClient: Api
+
+    var mHttpLoggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    var mOkHttpClient = OkHttpClient.Builder().addInterceptor(mHttpLoggingInterceptor).build()
+    var mRetrofit: Retrofit? = null
+
+    val client: Retrofit?
         get() {
-            val gson = GsonBuilder().setLenient().create()
-
-            val interceptor = HttpLoggingInterceptor()
-            interceptor.level = HttpLoggingInterceptor.Level.BODY
-
-            val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
-
-            val builder = Retrofit.Builder().baseUrl(BASE_URL).client(client).addConverterFactory(GsonConverterFactory.create(gson)).build()
-
-            return builder.create(Api::class.java)
+            if (mRetrofit == null) {
+                mRetrofit = Retrofit.Builder().baseUrl(Constant.BASE_URL).client(mOkHttpClient).addConverterFactory(GsonConverterFactory.create()).build()
+            }
+            return mRetrofit
         }
+
+    val getClient: UserApi
+        get() {
+
+            val gson = GsonBuilder().setLenient().create()
+            val interceptor = HttpLoggingInterceptor()
+
+            interceptor.level = HttpLoggingInterceptor.Level.BODY
+            val client = OkHttpClient.Builder().addInterceptor(interceptor).build()
+            val builder = Retrofit.Builder().baseUrl(BASE_URL).client(client).addConverterFactory(GsonConverterFactory.create(gson)).build()
+            return builder.create(UserApi::class.java)
+        }
+
 }
--- app/src/main/java/com/supplyop/driveop/login/network/UserApi.kt
+++ app/src/main/java/com/supplyop/driveop/login/network/UserApi.kt
@@ -1,17 +1,23 @@
 package com.supplyop.driveop.login.network
 
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface UserApi {
+    companion object {
+        fun getApi(): UserApi? {
+            return RetrofitInstance.client?.create(UserApi::class.java) }
+    }
 
     @POST("https://gateway.supplyop.com/supply-op/login/genrate-token")
     suspend fun loginUser(@Body loginRequest: LoginRequest): Response<LoginResponse>
 
-    companion object {
-        fun getApi(): UserApi? {
-            return ApiClient.client?.create(UserApi::class.java)
-        }
-    }
+    @GET("shipmentCurrent")
+    fun getShipmentCurrentList(): Call<List<ShipmentResponse>>
+
+    @GET("shipmentCompleted")
+    fun getShipmentCompletedList(): Call<List<ShipmentResponse>>
 }
